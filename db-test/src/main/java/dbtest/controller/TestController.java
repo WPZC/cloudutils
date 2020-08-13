3a0bddfe9da9bdd506ff72df0d952b3a25a56605
WPZC <1272722954@qq.com>
2020-08-13 18:47:44 +0800

    新增线程池，含有普通线程池和自定义线程池

--- db-test/src/main/java/dbtest/controller/TestController.java
+++ db-test/src/main/java/dbtest/controller/TestController.java
@@ -2,18 +2,22 @@ package dbtest.controller;
 
 import dbtest.entity.Test;
 import dbtest.service.TestService;
+import lombok.extern.slf4j.Slf4j;
+import methods.thread.ThreadFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author wqy
  * @version 1.0
  * @date 2020/8/10 17:35
  */
+@Slf4j
 @RestController
 public class TestController {
 
@@ -25,6 +29,26 @@ public class TestController {
     public List<Test> findByAll(){
 
         return testService.findByAll();
+
+    }
+
+    @RequestMapping(value = "/exce")
+    @ResponseBody
+    public void exce(){
+
+        ThreadFactory.threadPoolExecutorTask(new Runnable() {
+            @Override
+            public void run() {
+                log.info("我是一个粉刷匠");
+                //System.out.println("我是一个粉刷匠");
+            }
+        });
+        ThreadFactory.scheduleWithFixedDelay(new Runnable() {
+            @Override
+            public void run() {
+                log.info("我是第二个粉刷匠");
+            }
+        },5,10, TimeUnit.SECONDS);
 
     }
 
